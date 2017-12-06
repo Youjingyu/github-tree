0a91662f093a077f5ccbe3a881b27c47ce736c15
jingyu16 <jingyu16@staff>
2017-12-06 16:27:43 +0800

    custom project created by creat-react-app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TreeView from '../node_modules/react-treeview/lib/react-treeview';
+import '../node_modules/react-treeview/react-treeview.css';
+import './style/App.css';
+
+const dataSource = [
+  {
+    type: 'Employees',
+    collapsed: false,
+    people: [
+      {name: 'Paul Gordon', age: 29, sex: 'male', role: 'coder', collapsed: false},
+      {name: 'Sarah Lee', age: 27, sex: 'female', role: 'ocamler', collapsed: false},
+    ],
+  },
+  {
+    type: 'CEO',
+    collapsed: false,
+    people: [
+      {name: 'Drew Anderson', age: 39, sex: 'male', role: 'boss', collapsed: false},
+    ],
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {dataSource.map((node, i) => {
+          const type = node.type;
+          const label = <span className="node">{type}</span>;
+          return (
+            <TreeView key={type + '|' + i} nodeLabel={label} defaultCollapsed={false}>
+              {node.people.map(person => {
+                const label2 = <span className="node">{person.name}</span>;
+                return (
+                  <TreeView nodeLabel={label2} key={person.name} defaultCollapsed={false}>
+                    <div className="info">age: {person.age}</div>
+                    <div className="info">sex: {person.sex}</div>
+                    <div className="info">role: {person.role}</div>
+                  </TreeView>
+                );
+              })}
+            </TreeView>
+          );
+        })}
       </div>
     );
   }
