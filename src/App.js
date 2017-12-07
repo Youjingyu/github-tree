cdc3c0a8d7d516a4384ec8834bc3d355c397825a
jingyu16 <jingyu16@staff>
2017-12-07 13:49:03 +0800

    render app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,23 @@
 import React, { Component } from 'react';
-import TreeView from '../node_modules/react-treeview/lib/react-treeview';
-import '../node_modules/react-treeview/react-treeview.css';
+import {observer} from 'mobx-react';
 import './style/App.css';
+import { Tree } from './components';
+import treeStore from './store';
 
-const dataSource = [
-  {
-    type: 'Employees',
-    collapsed: false,
-    people: [
-      {name: 'Paul Gordon', age: 29, sex: 'male', role: 'coder', collapsed: false},
-      {name: 'Sarah Lee', age: 27, sex: 'female', role: 'ocamler', collapsed: false},
-    ],
-  },
-  {
-    type: 'CEO',
-    collapsed: false,
-    people: [
-      {name: 'Drew Anderson', age: 39, sex: 'male', role: 'boss', collapsed: false},
-    ],
-  },
-];
-
+@observer
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      treeData: []
+    }
+  }
   render() {
-    return (
-      <div>
-        {dataSource.map((node, i) => {
-          const type = node.type;
-          const label = <span className="node">{type}</span>;
-          return (
-            <TreeView key={type + '|' + i} nodeLabel={label} defaultCollapsed={false}>
-              {node.people.map(person => {
-                const label2 = <span className="node">{person.name}</span>;
-                return (
-                  <TreeView nodeLabel={label2} key={person.name} defaultCollapsed={false}>
-                    <div className="info">age: {person.age}</div>
-                    <div className="info">sex: {person.sex}</div>
-                    <div className="info">role: {person.role}</div>
-                  </TreeView>
-                );
-              })}
-            </TreeView>
-          );
-        })}
-      </div>
-    );
+    return (<Tree treeData={treeStore.treeData}/>)
+  }
+  componentWillMount(){
+    treeStore.setUrlPath('/Youjingyu/Code-Collection');
+    treeStore.getTreeData();
   }
 }
 
