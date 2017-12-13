e0a4b451ead4e635527f2632da9146d6af360d80
jingyu16 <jingyu16@staff>
2017-12-13 17:59:35 +0800

    allow user to config repo

--- src/App.js
+++ src/App.js
@@ -8,16 +8,45 @@ import treeStore from './store';
 class App extends Component {
   constructor(){
     super();
+    this.repo =
+      window.location.search && window.decodeURIComponent(window.location.search.split('=')[1]);
     this.state = {
-      treeData: []
+      repoPath: ''
     }
   }
   render() {
-    return (<Tree treeData={treeStore.treeData}/>)
+    if(this.repo){
+      return (<Tree treeData={treeStore.treeData}/>)
+    } else {
+      return (
+        <div>
+          <input type="text" value={this.state.repoPath}
+                 onChange={this.handleChange}
+                 onKeyUp={this.inputEnter}/>
+          <button onClick={this.setRepo}>确定</button>
+        </div>
+      )
+    }
   }
-  componentWillMount(){
-    treeStore.setUrlPath('/Youjingyu/Code-Collection');
-    treeStore.getTreeData();
+  componentDidMount(){
+    if(this.repo){
+      treeStore.setUrlPath(this.repo);
+      treeStore.getTreeData();
+    }
+  }
+  handleChange = (e)=>{
+    this.setState({
+      repoPath: e.target.value
+    });
+  }
+  inputEnter = (e)=>{
+    if(e.key === 'Enter'){
+      this.setRepo();
+    }
+  }
+  setRepo = ()=>{
+    window.location.search =
+      'repo=' + window.encodeURIComponent(this.state.repoPath.replace('https://github.com', ''));
   }
 }
 
